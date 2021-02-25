21d1ef361e0f3821f7b32238d5ac0433eb992124
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-02-25 22:52:47 +0530

    add attendee controller

---
+++ Backend/controllers/attendee_controller.js
@@ -0,0 +1,24 @@
+const Erur = require('../models/error');
+const { validationResult } = require('express-validator');
+const pool =require("../models/db_schema");
+
+const add_event = async(req,res,next)=>{
+    const { attendee_Name,attendee_Age,attendee_Number,attendee_Email,attendee_Address } =req.body;
+    try{
+        const new_attendee= await pool.query(
+            "INSERT INTO events_db(attendee_Name,attendee_Age,attendee_Number,attendee_Email,attendee_Address) VALUES($1,$2,$3,$4,$5) RETURNING *",
+            [attendee_Name,attendee_Age,attendee_Number,attendee_Email,attendee_Address]
+        );
+        console.log('success');
+        res.json(new_attendee);
+    }catch(err)
+    {
+        const error = new Erur(
+            'attendee entry halted',
+            422
+          );
+          return next(error);
+    }
+};
+
+exports.add_event=add_event;
