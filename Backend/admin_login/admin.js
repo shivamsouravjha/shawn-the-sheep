d1009a2ed19162a06c68a5b91e6f800cfd276b73
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-02-25 11:59:01 +0530

    Backend with Error,admin login and installs

---
+++ Backend/admin_login/admin.js
@@ -0,0 +1,1 @@
+const Erur = require('../models/error');
---
+++ Backend/app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.listen(process.env.PORT || 5000);
