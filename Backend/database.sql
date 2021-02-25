c4fc7d2074fcb13ee1684792ef066db38557edcc
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-02-25 16:36:09 +0530

    attendee_events database structure

--- Backend/database.sql
+++ Backend/database.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE data_base;
-CREATE TABLE eventsdb(
+CREATE TABLE events_db(
     events_id SERIAL PRIMARY KEY,
     event_name VARCHAR(255),
     event_date DATE
@@ -11,4 +11,9 @@ CREATE TABLE attendee_DB(
     attendee_Number smallint(0,10)  UNIQUE,
     attendee_Email  VARCHAR (50) UNIQUE,
     attendee_Address VARCHAR(510)
+);
+CREATE TABLE event_attendee_DB(
+    attendee_id integer(0,5) REFERENCES attendee_DB(attendee_id),
+    events_id integer(0,5) REFERENCES events_db(events_id),
+    PRIMARY KEY(attendee_id,events_id)
 );
