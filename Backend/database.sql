380feb35175e51e7d6282316dccc282f77fdb854
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-02-25 16:21:56 +0530

    attendee database structure

--- Backend/database.sql
+++ Backend/database.sql
@@ -4,3 +4,11 @@ CREATE TABLE eventsdb(
     event_name VARCHAR(255),
     event_date DATE
 );
+CREATE TABLE attendee_DB(
+    attendee_id SERIAL PRIMARY KEY,
+    attendee_Name VARCHAR(255),
+    attendee_Age smallint(0,3),
+    attendee_Number smallint(0,10)  UNIQUE,
+    attendee_Email  VARCHAR (50) UNIQUE,
+    attendee_Address VARCHAR(510)
+);
