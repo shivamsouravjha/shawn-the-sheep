3b2492f22d2470ccf228aeeda4cbb26d88d70392
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-02-27 19:17:21 +0530

    attendee form modified with error removed

--- Backend/models/db_schema.js
+++ Backend/models/db_schema.js
@@ -1,14 +1,9 @@
 const Pool =require("pg").Pool;
-require("dotenv").config();
-const offline_config = {
-    user: process.env.PG_user,
-    host:  process.env.PG_host,
-    database:  process.env.PG_database,
-    password:  process.env.PG_password,
-    port: process.env.PG_port
-};
-const production_config={
-    connectionString:process.env.Database_URL
-};
-const pool = new Pool( process.env.NODE_END === "production" ? production_config: offline_config);
-module.exports= pool;
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "data_base",
+    password: "123456",
+    port: "5432"
+});
+module.exports=pool;
--- frontend/src/attendee/user_work.js
+++ frontend/src/attendee/user_work.js
@@ -1,60 +1,63 @@
-import React, { Fragment, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './user_work.css';
  
 const  Form = ()=>{
-    const [name,setName]=useState("");
-    const [age,setAge]=useState("");
-    const [email,setEmail]=useState("");
-    const [contact,setContact]=useState("");
-    const [address,setAddress]=useState("");
-    const onSubmitform = async e=>{
+    const [attendee_Name,setName]=useState("");
+    const [attendee_Age,setAge]=useState("");
+    const [attendee_Email,setEmail]=useState("");
+    const [attendee_Number,setContact]=useState("");
+    const [attendee_Address,setAddress]=useState("");
+    const [events_id,setevents_id]=useState("7");
+    const onSubmitform = async e =>{
         e.preventDefault();
         try{
-            const body={name,age,email,contact,address};
-            console.log(name);
-            console.log(body);
+            const body={attendee_Name,attendee_Age,attendee_Number,attendee_Email,attendee_Address,events_id};
+            
+            const response = await fetch("http://localhost:5000/api/newattendee",{
+                method:"POST",headers:{"Content-Type":"application/json"},
+                body:JSON.stringify(body)
+            })
+            console.log(response);
         }catch(err){
             console.log('Error')
         }
     }
-    return (  
-        <Fragment> 
+    return (   
      <div className="attendee_form_div">
 		<h1 className="form_head">
-			Attendee Registration Form
-         </h1>
-		<p className="form_des">
-            Thankyou for your interest towards attending this event. Kindly fill the details to confirm your attendance for the event.
-        </p>
+			Attendee Registration Form </h1>
+		<p className="form_des">Thankyou for your interest towards attending this event. Kindly fill the details to confirm your attendance for the event.</p>
   <center>
 	<form  id="attendee_form"  name="attendee_form" onSubmit={onSubmitform}>
 			<label for="name" className="label_field">Name <span > * </span> </label><br/>
-				<input type="text" name="name" className="input_field" value={name} 
+				<input type="text" name="name" className="input_field" value={attendee_Name} 
                 placeholder="Name" onChange={e =>setName(e.target.value)} required />  <br/><br/>
 
         <label for="age" className="label_field"> Your Age <span > * </span> </label><br/>
-    		<input type="number" name="age" className="input_field" value={age} onChange={e =>setAge(e.target.value)} required /> <br/><br/>
+    		<input type="number" name="age" className="input_field" value={attendee_Age} onChange={e =>setAge(e.target.value)} required /> <br/><br/>
 
         <label for="contact" className="label_field"> Your contact number (Don't start with 0) <span > * </span> </label><br/>
     		<input type="text" name="contact" className="input_field" pattern="[1-9]{1}[0-9]{9}" required placeholder="your 10-digit number" 
-            value={contact} onChange={e =>setContact(e.target.value)}
+            value={attendee_Number} onChange={e =>setContact(e.target.value)}
             /> <br/><br/>
 
 		<label for="email" className="label_field"> Email <span > * </span> </label><br/>
     		<input type="email" className="input_field" name="email" 
-            value={email} onChange={e =>setEmail(e.target.value)} required /> <br/><br/>
+            value={attendee_Email} onChange={e =>setEmail(e.target.value)} required /> <br/><br/>
 
      	<label for="address" className="label_field"> Residential address <span > * </span> </label><br/>
      		<input type="text" name="address" required className="input_field" 
-             value={address} onChange={e =>setAddress(e.target.value)} required/> <br/><br/>
+             value={attendee_Address} onChange={e =>setAddress(e.target.value)} required/> <br/><br/>
      
   		
-		<button className="confirm_button" >Confirm</button>
-        </form>
-
+		<button type="submit" className="confirm_button" >Confirm</button>
+        </form> 
         </center>
-     </div>
-    </Fragment>
+
+
+
+
+    </div>
     );
   
 };
