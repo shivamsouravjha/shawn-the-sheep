065d093c0d1ff7f4d8d0ef6b0173797c28a361c8
shivamsouravjha <2019145@iiitdmj.ac.in>
2021-02-28 16:14:29 +0530

    deployment updtaes

--- models/db_schema.js
+++ models/db_schema.js
@@ -1,9 +1,15 @@
 const Pool =require("pg").Pool;
-const pool = new Pool({
+const devconfig = {
     user: "postgres",
     host: "localhost",
     database: "data_base",
     password: "123456",
     port: "5432"
-});
+}
+const proconfig = {
+    connectionString: process.env.DATABASE_URL
+}
+const pool = new Pool(
+    process.env.NODE_ENV === "production" ? proconfig:devconfig
+);
 module.exports=pool;
